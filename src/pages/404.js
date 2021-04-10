4c76fefba24c8850b4822ce1261eae563b80fa97
PouRia SA <pouriasa37@gmail.com>
2021-04-10 12:25:02 +0430

    v2.0 404Page added

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import * as styles from '../../SCSS/styles.module.scss'
 
 const NotFound = () => {
-    return ( <div>NOtFound</div> );
+    return (<div className={styles.notFoundPage}>
+        <h1>Error : 404</h1>
+        <h3>Page Not Found</h3>
+    </div>);
 }
- 
+
 export default NotFound;
